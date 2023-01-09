64557f4c0628c63c4941f0f940d58dfb4ba6582b
avseitsevad <103221369+avseitsevad@users.noreply.github.com>
2023-01-09 03:14:35 +0300

    Update Avseitseva_ZBPI211_LR3.py

--- 3 семестр/Avseitseva_ZBPI211_LR3.py
+++ 3 семестр/Avseitseva_ZBPI211_LR3.py
@@ -7,29 +7,27 @@ canvas = Canvas(root, width = size, height = size, bg = 'black')
 canvas.pack()
 gravitaion = 0.1 #величина с которой изменяется скорость падения при отскоке от пола
 
-class Ball():
+class Ball(): #создаём класс для частицы
     def __init__(self, canvas, radius = 30):
-        #создаём частицу
         self.canvas = canvas
         self.x0 = center - radius
         self.y0 = center - radius
         self.x1 = center + radius
         self.y1 = center + radius
-        self.speedY = 0
-        self.oval = canvas.create_oval(self.x0, self.y0, self.x1, self.y1, width = 3, fill = 'grey', outline = 'white')
+        self.speedY = 0 #собственной скорости у неё пока нет, но появится как только она начнёт падать
+        self.oval = canvas.create_oval(self.x0, self.y0, self.x1, self.y1, width = 3, fill = 'grey', outline = 'white') 
     def move(self):
-        self.speedY += gravitaion
-        self.y1 += self.speedY
-        self.canvas.move(self.oval, 0, self.speedY)
+        self.speedY += gravitaion #благодаря гравитации у частицы появляется скорость
+        self.y1 += self.speedY #координата по у меняется на полученную скорость
+        self.canvas.move(self.oval, 0, self.speedY) #двигаем частицу
         if self.y1 >= size and self.y1 - self.speedY <= size:
             self.speedY = -self.speedY #отскакиваем обратно при соприкосновении с полом
 
 
 ball = Ball(canvas)
 def redraw():
-    if ball.y1 + ball.speedY >= size and ball.y1 - ball.speedY >= size: 
+    if ball.y1 + ball.speedY >= size and ball.y1 - ball.speedY >= size: #вовремя выходим чтобы частица не начала бесконечно прыгать на границе окна
         return
-        #вовремя выходим чтобы частица не начала бесконечно прыгать на границе окна
     ball.move()
     root.after(10, redraw)
 
