b4798e9f3de6abaed80c353ac257317b563ad0bd
Dasha <davseitseva@gmail.com>
2023-05-27 08:23:24 +0300

    добавлен пп 4

--- 4 семестр/ЛР №3/client.py
+++ 4 семестр/ЛР №3/client.py
@@ -3,9 +3,23 @@
 
 import socket
 
+host_prompt = "Введите имя хоста (по умолчанию: localhost): "
+port_prompt = "Введите номер порта (по умолчанию: 9090): "
+
+default_host = 'localhost'
+default_port = '9090'
+
+host = input(host_prompt) or default_host
+port = input(port_prompt) or default_port
+port = int(port)
+
 sock = socket.socket()
-sock.connect(('localhost', 9090))
-print("Соединение с сервером...")
+try:
+    sock.connect(('localhost', 9090))
+    print("Соединение с сервером...")
+except ConnectionRefusedError:
+    print("Не удалось установить соединение с сервером. Проверьте правильность хоста и порта.")
+
 s = input("Введите строку для отправки: ")
 sock.send(s.encode())
 print("Отправка данных на сервер...")
--- 4 семестр/ЛР №3/server.py
+++ 4 семестр/ЛР №3/server.py
@@ -1,10 +1,23 @@
 import socket
 
+host_prompt = "Введите имя хоста (по умолчанию: localhost): "
+port_prompt = "Введите номер порта (по умолчанию: 9090): "
+
+default_host = 'localhost'
+default_port = '9090'
+
+host = input(host_prompt) or default_host
+port = input(port_prompt) or default_port
+port = int(port)
+
 sock = socket.socket()
-sock.bind(('', 9090))
-print("Запуск сервера...")
-sock.listen(0)
-print("Начало прослушивания порта...")
+try:
+    sock.bind(('', 9090))
+    print("Запуск сервера...")
+    sock.listen(0)
+    print("Начало прослушивания порта...")
+except OSError as e:
+    print(f"Не удалось запустить сервер. Проверьте правильность хоста и порта. Ошибка: {e}")
 
 while True:
     conn, addr = sock.accept()
