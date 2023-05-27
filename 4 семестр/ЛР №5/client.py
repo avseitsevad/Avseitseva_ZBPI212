d502057d1f545107de3142d1cf40bee014e41929
Dasha <davseitseva@gmail.com>
2023-05-27 10:44:09 +0300

    реализован протокол Диффи-Хеллмана

---
+++ 4 семестр/ЛР №5/client.py
@@ -0,0 +1,39 @@
+import socket
+import pickle
+
+host = 'localhost'
+port = 9090
+
+sock = socket.socket()
+sock.connect((host, port))
+
+p, g, a = 6, 5, 2
+
+# Вычисление публичного ключа клиента A
+A = (g ** a) % p
+
+# Отправка параметров p, g и публичного ключа A серверу
+sock.send(pickle.dumps((p, g, A)))
+
+# Получение публичного ключа сервера B
+B = pickle.loads(sock.recv(1024))
+
+# Вычисление общего секретного ключа K
+K = (B ** a) % p
+key = str(K)
+
+# Ввод сообщения с клавиатуры
+message = input("Введите сообщение для отправки на сервер: ")
+
+# Шифрование сообщения с использованием общего секретного ключа
+msg_encrypted = ""
+for i in range(len(message)):
+    char_code = ord(message[i])  # Получение кода символа
+    encrypted_code = char_code ^ int(key)  # Применение операции XOR с ключом
+    msg_encrypted += chr(encrypted_code)  # Преобразование обратно в символ
+
+# Отправка зашифрованного сообщения на сервер
+sock.send(pickle.dumps(msg_encrypted))
+print('Отправлено сообщение:', message)
+
+sock.close()
---
+++ 4 семестр/ЛР №5/server.py
@@ -0,0 +1,43 @@
+import socket
+import pickle
+
+host = 'localhost'
+port = 9090
+
+sock = socket.socket()
+sock.bind((host, port))
+sock.listen(1)
+conn, addr = sock.accept()
+
+# Получение параметров p, g и публичного ключа A от клиента
+p, g, A = pickle.loads(conn.recv(1024))
+
+# Генерация случайного секретного числа b для сервера
+b = 4
+
+# Вычисление публичного ключа сервера B
+B = (g ** b) % p
+
+# Отправка публичного ключа сервера B клиенту
+conn.send(pickle.dumps(B))
+
+# Вычисление общего секретного ключа K
+K = (A ** b) % p
+
+# Преобразование общего секретного ключа в строку
+key = str(K)
+
+# Получение зашифрованного сообщения от клиента
+msg_encrypted = pickle.loads(conn.recv(1024))
+print('Получено зашифрованное сообщение:', msg_encrypted)
+
+# Расшифровка сообщения с использованием общего секретного ключа
+msg_decoded = ""
+for i in range(len(msg_encrypted)):
+    encrypted_code = ord(msg_encrypted[i])  # Получение кода зашифрованного символа
+    decrypted_code = encrypted_code ^ int(key)  # Применение операции XOR с ключом
+    msg_decoded += chr(decrypted_code)  # Преобразование обратно в символ
+
+print('Расшифрованное сообщение:', msg_decoded)
+
+conn.close()
