da63c69a095fce6c576caae7a207bd21523b3359
avseitsevad <215726@edu.fa.ru>
2023-06-06 01:10:43 +0300

    пп1

--- 4 семестр/ЛР №5/Асимметричное шифрование/client.py
+++ 4 семестр/ЛР №5/Асимметричное шифрование/client.py
@@ -8,8 +8,9 @@ port = 8080
 sock = socket.socket()
 sock.connect((host, port))
 
-# Получение публичного ключа сервера
-server_pub_key = pickle.loads(sock.recv(1024))
+# Загрузка публичного ключа сервера из файла
+with open("server_public_key.txt", "rb") as f:
+    server_pub_key = pickle.load(f)
 
 # Ввод сообщения с клавиатуры
 message = input("Введите сообщение для отправки на сервер: ")
@@ -20,5 +21,9 @@ msg_encrypted = rsa.encrypt(message.encode(), server_pub_key)
 # Отправка зашифрованного сообщения на сервер
 sock.send(pickle.dumps(msg_encrypted))
 print('Отправлено сообщение:', message)
+
+# Получение ответа от сервера
+response = pickle.loads(sock.recv(1024))
+print('Получен ответ от сервера:', response)
 
 sock.close()
