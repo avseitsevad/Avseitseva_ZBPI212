a30528b678a9be82892c2d3b2fe6e671b3fde768
avseitsevad <215726@edu.fa.ru>
2023-06-06 11:46:27 +0300

    пп 4

--- 4 семестр/ЛР №7/server.py
+++ 4 семестр/ЛР №7/server.py
@@ -24,38 +24,40 @@ rename <oldname> <newname> - переименовывает файл с указ
 upload <filename> - копирует файл с клиента на сервер
 download <filename> - копирует файл с сервера на клиент
 login <username> <password> - выполняет авторизацию пользователя
+register <username> <password> - регистрирует нового пользователя
 '''
 
 # Указываем рабочую директорию для сервера
-dirname = os.path.join(os.getcwd(), 'docs')
-os.makedirs(dirname, exist_ok=True)
+server_dir = os.getcwd()
+users_dir = os.path.join(server_dir, 'users')
+os.makedirs(users_dir, exist_ok=True)
 
 # Файл с парами логин-пароль
-users_file = 'users.txt'
+users_file = os.path.join(server_dir, 'users.txt')
 
 def process(req):
     if req == 'pwd':
-        return dirname
+        return os.getcwd()
     elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
+        return '; '.join(os.listdir(os.getcwd()))
     elif req.startswith('cat'):
         _, filename = req.split(' ', 1)
         try:
-            with open(os.path.join(dirname, filename), 'r') as file:
+            with open(filename, 'r') as file:
                 return file.read()
         except FileNotFoundError:
             return 'File not found'
     elif req.startswith('mkdir'):
         _, dirname_new = req.split(' ', 1)
         try:
-            os.mkdir(os.path.join(dirname, dirname_new))
+            os.mkdir(dirname_new)
             return 'Directory created successfully'
         except FileExistsError:
             return 'Directory already exists'
     elif req.startswith('rmdir'):
         _, dirname_remove = req.split(' ', 1)
         try:
-            os.rmdir(os.path.join(dirname, dirname_remove))
+            os.rmdir(dirname_remove)
             return 'Directory removed successfully'
         except FileNotFoundError:
             return 'Directory not found'
@@ -64,14 +66,14 @@ def process(req):
     elif req.startswith('rm'):
         _, filename_remove = req.split(' ', 1)
         try:
-            os.remove(os.path.join(dirname, filename_remove))
+            os.remove(filename_remove)
             return 'File removed successfully'
         except FileNotFoundError:
             return 'File not found'
     elif req.startswith('rename'):
         _, oldname, newname = req.split(' ', 2)
         try:
-            os.rename(os.path.join(dirname, oldname), os.path.join(dirname, newname))
+            os.rename(oldname, newname)
             return 'File renamed successfully'
         except FileNotFoundError:
             return 'File not found'
@@ -81,14 +83,14 @@ def process(req):
         _, filename_upload = req.split(' ', 1)
         try:
             with open(filename_upload, 'rb') as file:
-                shutil.copyfileobj(file, open(os.path.join(dirname, os.path.basename(filename_upload)), 'wb'))
+                shutil.copyfileobj(file, open(os.path.join(os.getcwd(), os.path.basename(filename_upload)), 'wb'))
             return 'File uploaded successfully'
         except FileNotFoundError:
             return 'File not found'
     elif req.startswith('download'):
         _, filename_download = req.split(' ', 1)
         try:
-            with open(os.path.join(dirname, filename_download), 'rb') as file:
+            with open(filename_download, 'rb') as file:
                 return file.read()
         except FileNotFoundError:
             return 'File not found'
@@ -99,6 +101,12 @@ def process(req):
         else:
             logger.warning("Failed login attempt for user: %s", username)
             return 'Authentication failed'
+    elif req.startswith('register'):
+        _, username, password = req.split(' ', 2)
+        if register_user(username, password):
+            return 'Registration successful'
+        else:
+            return 'Registration failed'
     return 'bad request'
 
 def authenticate(username, password):
@@ -108,6 +116,16 @@ def authenticate(username, password):
             if username == stored_username and password == stored_password:
                 return True
     return False
+
+def register_user(username, password):
+    with open(users_file, 'a') as file:
+        file.write(f"{username},{password}\n")
+    
+    user_dir = os.path.join(users_dir, username)
+    os.makedirs(user_dir, exist_ok=True)
+    os.chdir(user_dir)
+    
+    return True
 
 
 PORT = 6666
