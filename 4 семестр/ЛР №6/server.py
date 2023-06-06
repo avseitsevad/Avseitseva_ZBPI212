b20d608b1c5af256153fe9f8ec08a00753178f6e
avseitsevad <215726@edu.fa.ru>
2023-06-06 03:03:31 +0300

    основные пункты

--- 4 семестр/ЛР №6/server.py
+++ 4 семестр/ЛР №6/server.py
@@ -1,13 +1,14 @@
 import socket
 
+def get_content(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
 sock = socket.socket()
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+sock.bind(('', 80))
+print("Using port 80")
 
 sock.listen(5)
 
@@ -19,12 +20,19 @@ msg = data.decode()
 
 print(msg)
 
-resp = """HTTP/1.1 200 OK
+request_parts = msg.split()
+if len(request_parts) > 1:
+    resource = request_parts[1]
+    if resource == '/':
+        resource = 'index.html'
+
+    content = get_content(resource)
+    resp = f"""HTTP/1.1 200 OK
 Server: SelfMadeServer v0.0.1
 Content-type: text/html
 Connection: close
 
-Hello, webworld!"""
+{content}"""
 
 conn.send(resp.encode())
 
