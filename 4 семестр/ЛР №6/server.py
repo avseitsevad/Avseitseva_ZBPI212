5ee7a020abe0a2627208cf8f78d54d66f6431006
avseitsevad <215726@edu.fa.ru>
2023-06-06 10:07:20 +0300

    пп1-5

--- 4 семестр/ЛР №6/server.py
+++ 4 семестр/ЛР №6/server.py
@@ -1,13 +1,10 @@
 import socket
 import os
 from datetime import datetime
+import threading
+from config import PORT, MAX_BYTES, PATH
 
-# Загрузка настроек из файла config.py
-try:
-    from config import PORT, MAX_BYTES, PATH
-except ImportError:
-    print("Error: config.py file not found.")
-    exit()
+LOG_FILE = "server_logs.txt"
 
 def get_content(file_path):
     # Чтение содержимого файла
@@ -30,25 +27,15 @@ def get_response(request):
                 return get_content(file_path)
     return ''
 
-sock = socket.socket()
-
-try:
-    sock.bind(('', PORT))
-    print("Using port", PORT)
-except OSError:
-    print("Error: Port", PORT, "is already in use.")
-    exit()
-
-sock.listen(5)
-
-while True:
-    conn, addr = sock.accept()
-    print("Connected", addr)
-
+def handle_request(conn, addr):
     data = conn.recv(MAX_BYTES)
     request = data.decode()
 
+    # Вывод информации о запросе в консоль
+    print("Received request from:", addr)
+    print("Request:")
     print(request)
+    print()
 
     response_content = get_response(request)
 
@@ -64,9 +51,48 @@ while True:
         ]
 
         response = "\r\n".join(headers) + "\r\n\r\n" + response_content
+        log_entry = "{}. {} - {} - 200".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), addr[0], request.split(' ')[1])
+
+        try:
+            conn.send(response.encode())
+        except socket.error:
+            print("Error: Failed to send response to", addr)
     else:
         # Отправка кода ошибки 404
         response = "HTTP/1.1 404 Not Found\r\nServer: SelfMadeServer v0.0.1\r\nConnection: close\r\n\r\n"
+        log_entry = "{}. {} - {} - 404".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), addr[0], request.split(' ')[1])
 
-    conn.send(response.encode())
+        try:
+            conn.send(response.encode())
+        except socket.error:
+            print("Error: Failed to send response to", addr)
+
     conn.close()
+
+    # Запись лога в файл
+    with open(LOG_FILE, 'a') as log_file:
+        log_file.write(log_entry + '\n')
+
+
+def start_server():
+    sock = socket.socket()
+
+    try:
+        sock.bind(('', PORT))
+        print("Using port", PORT)
+    except OSError:
+        print("Error: Port", PORT, "is already in use.")
+        exit()
+
+    sock.listen(5)
+
+    while True:
+        conn, addr = sock.accept()
+        print("Connected", addr)
+
+        # Запуск обработки запроса в отдельном потоке
+        thread = threading.Thread(target=handle_request, args=(conn, addr))
+        thread.start()
+
+if __name__ == '__main__':
+    start_server()
